5879a1afba58b7607d40f305989812d4fce3b52f
Mohit Chaudhary <cmmohitchau@gmail.com>
2024-07-04 21:55:44 +0545

    add minor project

---
+++ ch-04_functional_02/03_percentageFinder.py
@@ -0,0 +1,12 @@
+# percentageFinder
+
+amount = float(input("Enter your amount \n"))
+num = float(input("enter percentage number \n"))
+
+
+def percentageFinder(amount, num) :
+    print("Percentage finder app")
+    result = amount * num/100
+    print("Your result is" ,result)
+    
+percentageFinder(amount , num)
