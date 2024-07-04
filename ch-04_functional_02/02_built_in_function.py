dc4553cd054cc12e5a412cb1cb22a49a3741e403
Mohit Chaudhary <cmmohitchau@gmail.com>
2024-07-04 21:55:09 +0545

    add some built in function in my codebase

---
+++ ch-04_functional_02/02_built_in_function.py
@@ -0,0 +1,24 @@
+# int and float function
+x = "12.232"
+y = "33"
+
+z = float(x) + int(y)
+
+print(z)
+
+# string function
+a = 3
+b = 6
+
+c = str(a) + str(b)
+
+print(c)
+
+# input function
+
+name = input("Enter your name \n")
+print("Your name is" , name)
+
+num = int(input("Enter a number \n"))
+print(num + 12)
+
